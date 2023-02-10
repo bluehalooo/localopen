9ce0b6a32345cd6d7780e9ce5e402fdb5f3ac328
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-10 10:46:59 -0800

    whoah!

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
             let self = window;
             var x = window.open("");
-            var hello = prompt("Paste the link");
+            var index = prompt("Paste the link");
             self.close();
-            x.document.write("<iframe src=hello style='position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'></iframe>");
+            x.document.write("<iframe src=index style='position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;'></iframe>");
 //whoah.
